0eddb31828e7eb3d8ce6af91eb4c463edfcb0302
suhaAslam <suhaaslam2004@gmail.com>
2025-08-07 20:10:09 +0530

    Added Customer model with fields and Builder pattern

---
+++ src/main/java/com/pahanaedu/model/customer.java
@@ -0,0 +1,60 @@
+package com.pahanaedu.model;
+
+public class customer {
+    private int accountNumber;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private int unitsConsumed;
+
+    private customer(CustomerBuilder builder) {
+        this.accountNumber = builder.accountNumber;
+        this.name = builder.name;
+        this.address = builder.address;
+        this.phoneNumber = builder.phoneNumber;
+        this.unitsConsumed = builder.unitsConsumed;
+    }
+
+    public int getAccountNumber() { return accountNumber; }
+    public String getName() { return name; }
+    public String getAddress() { return address; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public int getUnitsConsumed() { return unitsConsumed; }
+
+    public static class CustomerBuilder {
+        private int accountNumber;
+        private String name;
+        private String address;
+        private String phoneNumber;
+        private int unitsConsumed;
+
+        public CustomerBuilder setAccountNumber(int accountNumber) {
+            this.accountNumber = accountNumber;
+            return this;
+        }
+
+        public CustomerBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public CustomerBuilder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public CustomerBuilder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public CustomerBuilder setUnitsConsumed(int unitsConsumed) {
+            this.unitsConsumed = unitsConsumed;
+            return this;
+        }
+
+        public customer build() {
+            return new customer(this);
+        }
+    }
+}
